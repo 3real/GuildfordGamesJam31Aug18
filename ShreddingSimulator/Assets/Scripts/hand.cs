ae16b9eae2c285ceda5a75ed5f0b79f0c78ea0bb
3real <mikegrahameverett@gmail.com>
2018-09-01 21:03:18 +0100

    minimum viable prodshreddy shredder shreding shreds

---
+++ ShreddingSimulator/Assets/Scripts/hand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hand : MonoBehaviour {
+
+public float speed = 1f;
+	public GameObject Paper;
+	private Rigidbody myRigidbody;
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	
+	void Update () {
+		
+	//if (Input.GetKeyDown (KeyCode.W)) {
+	//		myRigidbody.velocity = new Vector3 (0, speed, 0);
+	//	}
+	//}
+	float h = speed * Input.GetAxis("Vertical");
+	myRigidbody.velocity = new Vector3 (0,h,0);
+	}
+	
+}
+
---
+++ ShreddingSimulator/Assets/Scripts/lowerPaper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lowerPaper : MonoBehaviour {
+
+	public float speed = 1f;
+	public GameObject Paper;
+	private Rigidbody myRigidbody;
+
+	void Start () {
+		myRigidbody = this.gameObject.GetComponent<Rigidbody> ();
+	}
+
+void FirePaper () {
+    Rigidbody paperClone = Instantiate(Paper, transform.position, transform.rotation).GetComponent<Rigidbody>();
+
+}
+
+// Calls the fire method when holding down ctrl or mouse
+void Update () {
+    if (Input.GetButtonDown("Fire1")) {
+        FirePaper();
+    }
+}
+
+	
+	//void Update () {
+		
+	//if (Input.GetKeyDown (KeyCode.W)) {
+	//		myRigidbody.velocity = new Vector3 (0, speed, 0);
+	//	}
+	//}
+	//float h = speed * Input.GetAxis("Vertical");
+	//myRigidbody.velocity = new Vector3 (0,h,0);
+	//}
+	
+}
