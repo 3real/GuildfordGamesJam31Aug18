8873287a0ae5de8f2bf8abba022398661680ba92
3real <mikegrahameverett@gmail.com>
2018-09-01 17:10:11 +0100

    shredder shred shredy shreds added

---
+++ ShreddingSimulator/Assets/Scripts/spinnyShredder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinnyShredder : MonoBehaviour {
+
+	
+	public float speed = 500f;
+	public GameObject Shredder;
+	private Rigidbody myRigidbody;
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	
+	void Update () {
+		
+		//float h = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		//myRigidbody.AddTorque(transform.up * h);
+
+	if (Input.GetKeyDown(KeyCode.B))
+			{
+			//transform.RotateAround (Shredder.transform.position, Shredder.transform.forward, speed * Time.deltaTime);
+			myRigidbody.AddTorque(transform.forward * speed);
+			
+			}
+	}
+}
+
